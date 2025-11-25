0832db67473355b78ab2884d0f4362acd7a2f4df
Kuba <132784657+kubus08@users.noreply.github.com>
2025-11-25 14:58:14 +0100

    Update portfolio description from 'fast' to 'slow'

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-medium">Portfolio template!</h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
+          A clean, slow, and lightweight portfolio template built with Next.js,
           Vercel, and Tailwind CSS.
         </p>
         <p>
